301ea2896a64051aa8cb5e58eb88f83599cb6477
jaredl85 <jaredloveridge@gmail.com>
2021-12-02 12:48:18 -0800

    single movie page

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,9 +1,61 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { API_ENDPOINT } from "./context";
+const url =
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+//import useFetch from './useFetch';
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>;
+  const { id } = useParams();
+  //console.log(id);
+  const [movie, setMovie] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.Response === "false") {
+      setError({ show: true, msg: data.Error });
+      setLoading(false);
+    } else {
+      setMovie(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+
+  if (isLoading) {
+    return <div className="isLoading"></div>;
+  }
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+        <Link to="/" className="btn">
+          Back to movies
+        </Link>
+      </div>
+    );
+  }
+
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+  return (
+    <section className="single-movie">
+      <img src={poster === "N/A" ? url : poster} alt={title} />
+      <div className="single-movie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to="/" className="btn">
+          Back to movies
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default SingleMovie;
